0a00fb4044ab02bcade59c9e6382ce7f29e054c1
Abimael Garcia <Abimael.Garcia@improving.com>
2023-07-19 18:31:16 -0600

    get started with boilerplate project

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className='App'>
-      <h1 className='App-header'>Streametric</h1>
+      <h1 className='App-header'>My Boilerplate</h1>
     </div>
   );
 }
