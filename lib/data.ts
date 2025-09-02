124a8bd438b68dfb3f336416bb22040fa5b83a1f
Abimael Garcia <abimael1992g@gmail.com>
2025-09-02 17:33:16 -0600

    Actualización de productos a pesos mexicanos, nombres divertidos y rimados, descripciones mejoradas

--- lib/data.ts
+++ lib/data.ts
@@ -5,7 +5,7 @@ export const products: Product[] = [
 	{
 		id: 1,
 		name: 'Roll Clásico Risueño',
-		price: 4.99,
+		price: 50,
 		image: '/img/classic-cinnamon-roll-with-icing.png',
 		description:
 			'Nuestro rollo de canela emblemático que te hará reír de alegría',
@@ -13,8 +13,8 @@ export const products: Product[] = [
 	},
 	{
 		id: 2,
-		name: 'Roll de Chocolate Risueño',
-		price: 5.99,
+		name: 'Roll de Choco Risas',
+		price: 65,
 		image: '/img/chocolate-cinnamon-roll-with-chocolate-drizzle.png',
 		description: 'Doble chocolate delicioso que es seriamente divertido',
 		rating: 5,
@@ -22,15 +22,15 @@ export const products: Product[] = [
 	{
 		id: 3,
 		name: 'Remolino de Fresa',
-		price: 5.49,
+		price: 60,
 		image: '/img/strawberry-cinnamon-roll-with-pink-icing.png',
 		description: 'Giro dulce de fresa que te hará sonreír',
 		rating: 4.8,
 	},
 	{
 		id: 4,
-		name: 'Arándano Risueño',
-		price: 5.49,
+		name: 'Arándano en Verano',
+		price: 65,
 		image: '/img/blueberry-cinnamon-roll-with-cream-cheese-icing.png',
 		description: 'Repleto de arándanos y risas',
 		rating: 4.9,
@@ -38,7 +38,7 @@ export const products: Product[] = [
 	{
 		id: 5,
 		name: 'Glaseado Divertido',
-		price: 4.49,
+		price: 45,
 		image: '/img/glazed-cinnamon-roll-with-sugar-glaze.png',
 		description: 'Simple, dulce y garantizado para hacerte reír',
 		rating: 4.7,
@@ -46,7 +46,7 @@ export const products: Product[] = [
 	{
 		id: 6,
 		name: 'Caramelo Crujiente',
-		price: 6.49,
+		price: 70,
 		image: '/img/caramel-cinnamon-roll-with-crunchy-topping.png',
 		description: 'Delicioso caramelo crujiente que te hará reír a carcajadas',
 		rating: 5,
