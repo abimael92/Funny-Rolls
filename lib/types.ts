78d87e4d3f2b3e60dabdf05ecfbf0a73f3f3c44e
Abimael Garcia <abimael1992g@gmail.com>
2025-10-24 12:54:07 -0600

    add TypeScript types

--- lib/types.ts
+++ lib/types.ts
@@ -1,4 +1,5 @@
 // lib/types.ts
+// lib/types.ts
 export interface Product {
 	id: number;
 	name: string;
@@ -6,6 +7,32 @@ export interface Product {
 	image: string;
 	description: string;
 	rating: number;
+	available: boolean;
+	recipe: Recipe;
+}
+
+export interface Ingredient {
+	id: string;
+	name: string;
+	price: number;
+	unit: string;
+	amount: number;
+}
+
+export interface Recipe {
+	id: string;
+	name: string;
+	ingredients: RecipeIngredient[];
+	batchSize: number;
+	sellingPrice: number;
+	profitMargin: number;
+	available: boolean;
+	steps: string[];
+}
+
+export interface RecipeIngredient {
+	ingredientId: string;
+	amount: number;
 }
 
 export interface CartItem extends Product {
