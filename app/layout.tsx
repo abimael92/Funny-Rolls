0fa1f8287ce2a19c527335e792a1094daddf1c5c
Abimael Garcia <abimael1992g@gmail.com>
2026-03-08 03:04:29 -0600

    refactor layout and navbar structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,43 +1,36 @@
-import type React from "react"
-import { Pacifico, Inter } from "next/font/google"
-import "./globals.css"
+import type { Metadata } from "next";
+import { Pacifico, Inter } from "next/font/google";
+import { AuthProvider } from './providers/AuthProvider'
+import "./globals.css";
 
 const pacifico = Pacifico({
   weight: "400",
   subsets: ["latin"],
-  display: "swap",
   variable: "--font-pacifico",
-})
+});
 
 const inter = Inter({
   subsets: ["latin"],
-  display: "swap",
   variable: "--font-inter",
-})
+});
 
-export const metadata = {
-    title: "Funny Rolls - Risas y Rollos de Canela",
-    description:
-      "Deliciosos rollos de canela que te harán reír y querer más. ¡Ordénalos ahora y disfruta el antojo perfecto!",
-    icons: {
-    icon: [
-      { url: '/favicon/favicon-32x32.png', type: 'image/png', sizes: '32x32' },
-      { url: '/favicon/favicon-16x16.png', type: 'image/png', sizes: '16x16' }
-    ],
-    apple: '/favicon/apple-touch-icon.png',
-    shortcut: '/favicon/favicon.ico',
-  },
-  manifest: '/favicon/site.webmanifest'
-}
+export const metadata: Metadata = {
+  title: "Funny Rolls - Risas y Rollos de Canela",
+  description: "Deliciosos rollos de canela",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={`${pacifico.variable} ${inter.variable} antialiased`}>
-      <body>{children}</body>
+    <html lang="en" className={`${pacifico.variable} ${inter.variable}`}>
+      <body suppressHydrationWarning={true}>
+        <AuthProvider>
+          {children}
+        </AuthProvider>
+      </body>
     </html>
-  )
+  );
 }
