be7bc81bcb0604074025bc5b35f655adef1cedef
Abimael Garcia <abimael1992g@gmail.com>
2026-03-08 03:05:12 -0600

    feat auth API endpoints

---
+++ app/api/auth/logout/route.ts
@@ -0,0 +1,14 @@
+import { createClient } from '@supabase/supabase-js';
+import { cookies } from 'next/headers';
+import { NextResponse } from 'next/server';
+
+export async function POST() {
+	const supabase = createClient(
+		process.env.NEXT_PUBLIC_SUPABASE_URL!,
+		process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+	);
+
+	await supabase.auth.signOut();
+
+	return NextResponse.json({ success: true });
+}
